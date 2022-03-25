a8f24978aa41492f3a44bdb4e9f6e9e31c631f92
Dhrupal Kamlesh Desai <desai.dhrupal15@gmail.com>
2022-03-25 15:05:12 -0400

    Added logo, launch screen, login page ui

--- Expense Tracker/LoginViewController.swift
+++ Expense Tracker/LoginViewController.swift
@@ -8,22 +8,30 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var userTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signupButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        userTextField.layer.cornerRadius = 20
+        passwordTextField.layer.cornerRadius = 20
+        loginButton.layer.cornerRadius = 20
+        signupButton.layer.cornerRadius = 20
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
