93441b6d038db8f83ebaf56d397a21a97acf5574
Dhrupal Kamlesh Desai <desai.dhrupal15@gmail.com>
2022-04-17 11:22:45 -0400

    UI navigation bar color change

--- Expense Tracker/AppDelegate.swift
+++ Expense Tracker/AppDelegate.swift
@@ -15,10 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         // Set Tab Bar appearance for selected and Un selected tab color
-        UITabBar.appearance().tintColor = UIColor.white
+        //UITabBar.appearance().tintColor = UIColor.white
         UITabBar.appearance().isTranslucent = false
         
-        UINavigationBar.appearance().tintColor = UIColor.white
+        //UINavigationBar.appearance().tintColor = UIColor.white
         
         return true
     }
